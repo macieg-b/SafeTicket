78df20f3972f747dccf61426b6e0d0a7573b5fbe
Maciej Bartłomiejczyk <Maciej Bartomiejczyk>
2016-05-04 13:16:16 +0200

    Added crud.py

---
+++ crud.py
@@ -0,0 +1,17 @@
+from flask import jsonify, json
+import MySQLdb
+import peewee
+from peewee import *
+
+def add_record(arg):
+	data=jsonify(arg)
+	longitude=arg['x']
+	lattitude=arg['y']
+	print "***************\nCoordinates: \n\n"+longitude+"\n"+lattitude+"\n***************"
+
+	db = MySQLdb.connect(host="us-cdbr-azure-east-a.cloudapp.net",
+                 	     user="baa57d8d1e6f72",
+                	     passwd="4cb8825d",
+                             db="YanosikDB")
+	cur=db.cursor()
+	cur.execute("INSERT INTO cords (id, longitude, latitude) VALUES (6, 11.11, 22.22)")
