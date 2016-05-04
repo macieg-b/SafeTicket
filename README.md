a9cf0f011ea42e4fdaa827e31263273c3f72c31e
Maciej Bartłomiejczyk <Maciej Bartomiejczyk>
2016-05-04 13:12:51 +0200

    Added server.py and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+#Small webserver written in Python, for educational use only. Provides api for mobile application. 
+
+Based on BaseHTTPServer.
---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import crud
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	 return render_template('index.html')
+
+@app.route('/api/post', methods = ['POST'])
+def post():
+	json = request.json
+	crud.add_record(json)
+	return 
+
+
+if __name__ == '__main__':
+	app.run(port=8000)
