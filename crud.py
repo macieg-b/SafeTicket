19c519435bc8c7159e735d44c09e6baeb61ca841
macieg_b <bartlomiejczyk.maciej@gmail.com>
2016-05-07 18:49:05 +0200

    changed crud

--- crud.py
+++ crud.py
@@ -1,15 +1,23 @@
 from flask import jsonify, json
 import MySQLdb
 
-def add_record(arg):
-	data=jsonify(arg)
+hostData="us-cdbr-azure-east-a.cloudapp.net";
+userData="baa57d8d1e6f72";
+passData="4cb8825d";
+dbData="YanosikDB";
+
+def register(jsonArg):
+	#Database connection, and select object to send queries
+	db = MySQLdb.connect(host=hostData,
+                 	     user=usetData,
+                	     passwd=passData,
+                    	 db=dbData)
+	cur=db.cursor()
+
+	#Parse jsonArg to proper variables
 	longitude=arg['x']
 	lattitude=arg['y']
-	print "***************\nCoordinates: \n\n"+longitude+"\n"+lattitude+"\n***************"
 
-	db = MySQLdb.connect(host="us-cdbr-azure-east-a.cloudapp.net",
-                 	     user="baa57d8d1e6f72",
-                	     passwd="4cb8825d",
-                             db="YanosikDB")
-	cur=db.cursor()
+	#Execute proper queries
 	cur.execute("INSERT INTO cords (id, longitude, latitude) VALUES (6, 11.11, 22.22)")
+return
