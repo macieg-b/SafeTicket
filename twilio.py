647515edb8a96e829021df1e1145cc7b90d96ab8
macieg-b <bartlomiejczyk.maciej@gmail.com>
2016-05-08 17:35:29 +0200

    Delete twilio.py

--- twilio.py
+++
@@ -1,8 +0,0 @@
-	# Your Account Sid and Auth Token from twilio.com/user/account
-	account_sid = "AC122b62b4982017c9239580ad0bc21e2c"
-	auth_token  = "6e00535423c7711d86543125d4386fb1"
-	client = TwilioRestClient(account_sid, auth_token)
-	message = client.messages.create(body="\nMB - The best programmer ever",
-	    to="+48691669003",    # Replace with your phone number
-	    from_="+48732483582") # Replace with your Twilio number
-	print message.sid
