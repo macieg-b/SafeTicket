24d536d236037129bcc881c9350b279a2b14cacd
wsambor <waldeksambor@gmail.com>
2016-05-12 16:11:15 +0000

    Update .gitignore and stop_server script

--- stop_server.sh
+++ stop_server.sh
@@ -13,11 +13,18 @@ KILL_SECOND_PROCESS=$(ps axf | \
 	awk '{print "kill -9 " $1}' |\
 	sh)
 
+ADD_LOG_TO_ARCHIVE=$(cat nohup.out >> log_archive.log)
+
+REMOVE_LOG_FILE=$(rm nohup.out)
+
 if [ $PROCESS_NUM > 1 ]; then
 	$KILL_FIRST_PROCESS
 	echo "First stopped"
 	$KILL_SECOND_PROCESS
 	echo "Second stopped"
+	$ADD_LOG_TO_ARCHIVE
+	$REMOVE_LOG_FILE
+	$(rm "1")
 else
 	echo "Server is not already running"
 fi
