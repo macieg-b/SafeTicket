0009a6cd8002db2dcae6127342a8a32f06e7976c
wsambor <waldeksambor@gmail.com>
2016-05-28 10:52:07 +0000

    Code refactoring

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import json
 import crud
-import showDB
+import show_db
 app = Flask(__name__)
 
 @app.route('/')
---
+++ show_db.py
@@ -0,0 +1,12 @@
+from setting import hostData, userData, passData, dbData
+import MySQLdb
+
+def SelectAll():
+    db = MySQLdb.connect(host = hostData, user = userData, passwd = passData, db = dbData)
+    cur = db.cursor()
+
+    cur.execute("SELECT `phone`, `1time_code` FROM `USERS`")
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+    return result
